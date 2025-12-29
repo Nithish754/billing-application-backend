1d81db0cf066ec021ea9a6d1961c5b1438844591
nithish <nnnithish754@gmail.com>
2025-12-29 20:33:19 +0530

    webconfig changes

--- demo/src/main/java/com/mrk/demo/config/WebConfig.java
+++ demo/src/main/java/com/mrk/demo/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins(
+                .allowedOriginPatterns(
                         "http://localhost:5173",
                         "https://*.vercel.app"
                 )
